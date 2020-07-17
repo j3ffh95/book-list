05dc8003abb75f2e6e0954ece7830f17e2ed41fa
j3ffh95 <j3ffh95@gmail.com>
2020-07-16 23:07:47 -0400

    Created event listener for the deleting a book

--- appes6.js
+++ appes6.js
@@ -85,3 +85,15 @@ document.getElementById("book-form").addEventListener("submit", function (e) {
 
   e.preventDefault();
 });
+
+// Event Listener for delete
+document.getElementById("book-list").addEventListener("click", function (e) {
+  // Instantiate UI
+  const ui = new UI();
+  // Delete book
+  ui.deleteBook(e.target);
+  // Show message
+  ui.showAlert("Book removed!", "success");
+
+  e.preventDefault();
+});
