5c78dc6c184d81bc54ba58f48d2fd9190d24db0d
j3ffh95 <j3ffh95@gmail.com>
2020-07-16 23:36:25 -0400

    Created displayBooks method

--- appes6.js
+++ appes6.js
@@ -64,11 +64,27 @@ class Store {
     } else {
       books = JSON.parse(localStorage.getItem("books"));
     }
+
+    return books;
   }
 
-  static displayBooks() {}
+  static displayBooks() {
+    const books = Store.getBooks();
 
-  static addBook() {}
+    books.forEach((book) => {
+      const ui = new UI();
+      // Add book to UI
+      ui.addBookToList(book);
+    });
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+
+    books.push(book);
+
+    localStorage.setItem("books", JSON.stringify(books));
+  }
 
   static removeBook() {}
 }
