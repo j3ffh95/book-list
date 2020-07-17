010bf4701ccdd065924a31fd74026b23e17bed6f
j3ffh95 <j3ffh95@gmail.com>
2020-07-16 23:03:13 -0400

    Added addbooktolist method in UI class

--- appes6.js
+++ appes6.js
@@ -6,4 +6,18 @@ class Book {
   }
 }
 
-class UI {}
+class UI {
+  addBookToList(book) {
+    const list = document.getElementById("book-list");
+    // Create tr element
+    const row = document.createElement("tr");
+    // Insert cols
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
+  `;
+    // Append row to list
+  }
+}
