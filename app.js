b842278e2cc71056ac095cae44fe909898522a16
j3ffh95 <j3ffh95@gmail.com>
2020-07-16 22:18:48 -0400

    Created addBookToList function in the UI constructor

---
+++ app.js
@@ -0,0 +1,43 @@
+// Book Constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+// UI Constructor
+function UI() {}
+
+UI.prototype.addBookToList = function (book) {
+  const list = document.getElementById("book-list");
+  // Create tr element
+  const row = document.createElement("tr");
+  // Insert cols
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
+  `;
+  // Append row to list
+  list.appendChild(row);
+};
+
+// Event Listeners
+document.getElementById("book-form").addEventListener("submit", function (e) {
+  // Get form values
+  const title = document.getElementById("title").value,
+    author = document.getElementById("author").value,
+    isbn = document.getElementById("isbn").value;
+
+  // Instantiate a book
+  const book = new Book(title, author, isbn);
+
+  // Instantiate a ui
+  const ui = new UI();
+
+  // Add book to list
+  ui.addBookToList(book);
+
+  e.preventDefault();
+});
